3fa548836f4e4b89654270de8e2d4834830681e6
leoocestari <leonardocestari67@gmail.com>
2024-10-17 21:11:44 -0300

    left-sidebar

---
+++ src/app/modules/components/left-sidebar/left-sidebar.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { Component, input, output } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-left-sidebar',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+  templateUrl: './left-sidebar.component.html',
+  styleUrl: './left-sidebar.component.scss',
+})
+export class LeftSidebarComponent {
+  isLeftSidebarCollapsed = input.required<boolean>();
+  changeIsLeftSidebarCollapsed = output<boolean>();
+  items = [
+    {
+      routeLink: 'dashboard',
+      icon: 'fal fa-calendar',
+      label: 'Agendamentos',
+    },
+    {
+      routeLink: 'products',
+      icon: 'fal fa-user',
+      label: 'Pacientes',
+    },
+    {
+      routeLink: 'pages',
+      icon: 'fal fa-cog',
+      label: 'Suporte',
+    },
+    
+  ];
+
+  toggleCollapse(): void {
+    this.changeIsLeftSidebarCollapsed.emit(!this.isLeftSidebarCollapsed());
+  }
+
+  closeSidenav(): void {
+    this.changeIsLeftSidebarCollapsed.emit(true);
+  }
+}
