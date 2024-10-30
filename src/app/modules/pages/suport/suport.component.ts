67cee578061758f6d9eea25c7fb93df8a99604bc
caio bonetto prazeres <caioprazeres1@gmail.com>
2024-10-29 21:21:49 -0300

    form support

--- src/app/modules/pages/suport/suport.component.ts
+++ src/app/modules/pages/suport/suport.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-suport',
@@ -8,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrl: './suport.component.scss'
 })
 export class SuportComponent {
+  supportForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.supportForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      department: ['', Validators.required],
+      description: ['', Validators.required],
+      errorText: ['']
+    });
+  }
+
+  onSubmit() {
+    if (this.supportForm.valid) {
+      console.log('Formulário enviado com sucesso:', this.supportForm.value);
+      // Adicione a lógica de envio aqui, como integração com uma API ou serviço.
+    } else {
+      console.log('Formulário inválido.');
+    }
+  }
 }
