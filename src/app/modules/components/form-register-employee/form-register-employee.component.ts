f12d5db568ced41d61f9a8a360e2d2fed3aa1f83
caio bonetto prazeres <caioprazeres1@gmail.com>
2024-11-11 19:56:11 -0300

    create and edit consult and register

--- src/app/modules/components/form-register-employee/form-register-employee.component.ts
+++ src/app/modules/components/form-register-employee/form-register-employee.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, ViewChild, ChangeDetectorRef, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-register-employee',
   standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush,  // Mudança aqui
   imports: [
     CommonModule,
     FormsModule,
@@ -14,18 +13,18 @@ import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angul
   templateUrl: './form-register-employee.component.html',
   styleUrls: ['./form-register-employee.component.scss']
 })
-export class FormRegisterEmployeeComponent implements OnInit {
+export class FormRegisterEmployeeComponent {
   isExpanded: boolean = false; // Variável de controle
   registerEmployeeForm: FormGroup;
 
   // Referência ao input de arquivo
   @ViewChild('fileInput', { static: false }) fileInput!: ElementRef;
 
-  constructor(private fb: FormBuilder, private cdr: ChangeDetectorRef) {
+  constructor(private fb: FormBuilder) {
     // Inicialização do formulário
     this.registerEmployeeForm = this.fb.group({
       name: [''],
-      position: [''],
+      lastname: [''],
       birthDate: [''],
       email: [''],
       rg: [''],
@@ -36,21 +35,15 @@ export class FormRegisterEmployeeComponent implements OnInit {
     });
   }
 
-  ngOnInit() {
-    console.log('Estado inicial de isExpanded em FormRegisterEmployee:', this.isExpanded);
-  }
-
   // Função para alternar entre expandido/retraído
   toggleForm() {
     this.isExpanded = !this.isExpanded;
-    console.log('isExpanded após clique:', this.isExpanded);
-    this.cdr.markForCheck(); // Marca para detecção de mudanças
   }
 
   // Lógica ao submeter o formulário
   onSubmit() {
     if (this.registerEmployeeForm.valid) {
-      console.log('Funcionário registrado:', this.registerEmployeeForm.value);
+      console.log('Paciente registrado:', this.registerEmployeeForm.value);
     }
   }
 
--- src/app/modules/pages/consultar/consultar.component.ts
+++ src/app/modules/pages/consultar/consultar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ConsultComponent } from "../../components/consult/consult.component";
+import { ConsultEmployeeComponent } from "../../components/consult-employee/consult-employee.component";
 
 @Component({
   selector: 'app-consultar',
   standalone: true,
-  imports: [ConsultComponent],
+  imports: [ConsultComponent, ConsultEmployeeComponent],
   templateUrl: './consultar.component.html',
   styleUrl: './consultar.component.scss'
 })
