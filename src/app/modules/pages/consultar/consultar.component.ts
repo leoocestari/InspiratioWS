743825aa0b1bd10a8fa366f7ba8caaf6bcfd861c
caio bonetto prazeres <caioprazeres1@gmail.com>
2024-11-18 18:26:07 -0300

    add barra pesquisa em consultas

--- src/app/modules/pages/consultar/consultar.component.ts
+++ src/app/modules/pages/consultar/consultar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { ConsultComponent } from "../../components/consult/consult.component";
 import { ConsultEmployeeComponent } from "../../components/consult-employee/consult-employee.component";
+import { BarraPesquisaComponent } from '../../components/barra-pesquisa/barra-pesquisa.component';
 
 @Component({
   selector: 'app-consultar',
   standalone: true,
-  imports: [ConsultComponent, ConsultEmployeeComponent],
+  imports: [
+    BarraPesquisaComponent,
+    ConsultComponent, 
+    ConsultEmployeeComponent
+  ],
   templateUrl: './consultar.component.html',
   styleUrl: './consultar.component.scss'
 })
